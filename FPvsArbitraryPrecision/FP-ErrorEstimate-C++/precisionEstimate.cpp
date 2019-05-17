7ca19c80179f2cb8d7345b75411eae16454c58da
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-05-17 12:58:36 -0400

    Added readme file and cleaned up C++ FP Cleve Moler Example

--- FPvsArbitraryPrecision/FP-ErrorEstimate-C++/precisionEstimate.cpp
+++ FPvsArbitraryPrecision/FP-ErrorEstimate-C++/precisionEstimate.cpp
@@ -5,22 +5,24 @@
 
 #include "cleveMoler.h"
 
+using namespace std;
 
 /**
  * Run an arbitrary function a predefined number of times.
  *
  * @param f function to run
- * @param num_execs number of function executions
+ * @param numExecs number of function executions
  *
  * @return total execution time
  */
 template<typename T>
-long performExecs(T (*f)(), long num_execs)
+long performExecs(T (*f)(), long numExecs)
 {
-    long start = time(NULL);
     T p;
 
-    for (long i = 0; i < num_execs ; i++) {
+    long start = time(NULL);
+
+    for (long i = 0; i < numExecs; i++) {
         p = f();
     }
 
@@ -33,31 +35,24 @@ long performExecs(T (*f)(), long num_execs)
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " num_execs" << "\n";
+        std::cerr << "Usage: " << argv[0] << " numExecs" << "\n";
         return 1;
     }
 
-    long num_execs = std::stol(argv[1]);
+    long numExecs = std::stol(argv[1]);
 
-    float sp;
-    double dp;
-    float128 qp; // float128 is added by boost
+    float    sp = estimatePrecision<float>();
+    double   dp = estimatePrecision<double>();
+    float128 qp = estimatePrecision<float128>();
 
-    /*
-    std::cout << sp << "\n"
-              << dp << "\n"
-              << qp << "\n";
-    */
+    long totalTime = performExecs(estimatePrecision<float>, numExecs);
+    cout << right << setw(4) <<  totalTime << " secs | " << sp << "\n";
 
-    std::cout << performExecs(estimatePrecision<float>, num_execs) << " secs"
-              << " | "
-              << num_execs << " # executions" << "\n";
-    std::cout << performExecs(estimatePrecision<double>, num_execs) << " secs"
-              << " | "
-              << num_execs << " # executions" << "\n";
-    std::cout << performExecs(estimatePrecision<float128>, num_execs) << " secs"
-              << " | "
-              << num_execs << " # executions" << "\n";
+    totalTime = performExecs(estimatePrecision<double>, numExecs);
+    cout << right << setw(4) <<  totalTime << " secs | " << dp << "\n";
+
+    totalTime = performExecs(estimatePrecision<float128>, numExecs);
+    cout << right << setw(4) <<  totalTime << " secs | " << qp << "\n";
 
     return 0;
 }

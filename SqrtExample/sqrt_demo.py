c4d8a5b57af22897ff3a1f351488986073c427de
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2020-05-18 11:55:14 -0400

    Add README.md to sqrt example

---
+++ SqrtExample/sqrt_demo.py
@@ -0,0 +1,9 @@
+#! /usr/bin/eny python
+
+import math
+
+if __name__ == "__main__":
+
+    for power in range(0, 20):
+        result = math.sqrt(1 + 10 ** -power)
+        print(f" sqrt(1 + 10^-{power:<2d}) = {result:>50.50f}")

bfba4cc4a60c96b27c100ae83afdccf135c95769
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2021-05-26 20:29:56 -0400

    Add Arbitrary precision discussion code examples

---
+++ ArbitraryPrecisionDiscussion2/Cpp/math_example.h
@@ -0,0 +1,9 @@
+#ifndef MATH_EXAMPLE_H_INCLUDED
+#define MATH_EXAMPLE_H_INCLUDED
+
+/**
+ * An example function used to demonstrate templates.
+ */
+double g(double x);
+
+#endif
---
+++ ArbitraryPrecisionDiscussion2/Python/close_to_zero.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Add module doc
+
+"""
+
+import math
+import typing
+
+from typing import Callable
+
+
+def approach_zero(f: Callable[[float], float]):
+    """
+    Evaluate a given function (f) for x values in the range 1 to 2^(-2000)
+    where each value is have the previous one (1, 0.5, 0.25, 0.125...)
+
+    Args:
+        f: mathematical function to evaluate
+    """
+
+    for i in range(0, 2000):
+
+        x = 2**-i
+        f_of_x = f(x)
+
+        #  print("2^-{} / {:>24.20e} | {:>24.20e}".format(i, x, f_of_x))
+        print(f"2^-{i:} / {x:>24.20e} | {f_of_x:>24.20e}")
+
+
+if __name__ == "__main__":
+
+    def f(x: float):
+        return x**2 + 7 * x + 3
+
+
+    #  approach_zero(lambda x: math.cos(x))
+    approach_zero(f)

776f0848ba6ba02628cc2925e9d1095f255592d9
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-11-09 19:34:29 -0500

    Update Rust input library to use LazyCell and thiserror

--- SemesterProject-CPU-Temps/Rust/temperature-parser/src/lib.rs
+++ SemesterProject-CPU-Temps/Rust/temperature-parser/src/lib.rs
@@ -1,10 +1,10 @@
-use lazy_static::lazy_static;
 use regex::Regex;
-use std::convert::From;
+use std::cell::LazyCell;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::fmt;
+
+use thiserror::Error;
 
 #[derive(Debug)]
 pub struct TemperatureLine {
@@ -12,41 +12,23 @@ pub struct TemperatureLine {
     pub readings: Vec<f64>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ParseError {
-    IOError(std::io::Error),
-}
-
-impl From<std::io::Error> for ParseError {
-    fn from(err: std::io::Error) -> Self {
-        ParseError::IOError(err)
-    }
-}
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match &self {
-            ParseError::IOError(err) => {
-                write!(f, "{:?}", err)
-            }
-        }
-    }
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
 }
 
 //------------------------------------------------------------------------------
 const TIME_STEP_SIZE: u64 = 30;
 
-lazy_static! {
-    static ref LINE_DELIM_RE: Regex = Regex::new(r"[^0-9]*\s+|[^0-9]*$").unwrap();
-}
+const LINE_DELIM_RE: LazyCell<Regex> =
+    LazyCell::new(|| Regex::new(r"[^0-9]*\s+|[^0-9]*$").unwrap());
 
 pub fn read_temperature_file(filename: &str) -> Result<Vec<TemperatureLine>, ParseError> {
     let file = File::open(filename)?;
-
     let reader = BufReader::new(file);
 
-    let readings = read_temperatures(reader);
-    readings
+    read_temperatures(reader)
 }
 
 pub fn read_temperatures<R>(reader: R) -> Result<Vec<TemperatureLine>, ParseError>
@@ -56,27 +38,21 @@ where
     let mut readings: Vec<TemperatureLine> = Vec::new();
 
     for (idx, wrapped_line) in reader.lines().enumerate() {
-        match wrapped_line {
-            Ok(line) => {
-                let time = (idx as u64) * TIME_STEP_SIZE;
+        let line = wrapped_line?;
+        let time = (idx as u64) * TIME_STEP_SIZE;
 
-                let core_temps: Vec<f64> = LINE_DELIM_RE
-                    .split(line.trim())
-                    .into_iter()
-                    .filter_map(|s| s.parse().ok())
-                    .collect();
+        let core_temps: Vec<f64> = LINE_DELIM_RE
+            .split(line.trim())
+            .into_iter()
+            .filter_map(|s| s.parse().ok())
+            .collect();
 
-                let new_reading = TemperatureLine {
-                    time_step: time,
-                    readings: core_temps,
-                };
+        let new_reading = TemperatureLine {
+            time_step: time,
+            readings: core_temps,
+        };
 
-                readings.push(new_reading);
-            }
-            Err(err) => {
-                return Err(err.into());
-            }
-        }
+        readings.push(new_reading);
     }
 
     Ok(readings)

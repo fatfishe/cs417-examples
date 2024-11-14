73acfc2ba2f55194fa508997e87aee21312852af
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-11-14 16:49:00 -0500

    Rename examples

---
+++ rename.sh
@@ -0,0 +1,6 @@
+mv 10-LeastSquaresApproximation-Python 10-Least-Squares-Approximation-Python
+mv 20-DividedDifferences-Python 20-Divided-Differences-Python
+mv 30-NonLinearEquationSolvers-Python 30-Non-Linear-Equation-Solvers-Python
+mv 30-NonLinearEquationSolvers-Rust 30-Non-Linear-Equation-Solvers-Rust
+mv 40-MonteCarloIntegration-Python 40-Monte-Carlo-Integration-Python
+mv 40-MonteCarloIntegration-Rust 40-Monte-Carlo-Integration-Rust

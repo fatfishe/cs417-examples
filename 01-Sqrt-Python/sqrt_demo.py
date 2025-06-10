ff5ea1d5d2ef9abe536f5bab8fe56d680f168985
fatfishe <49493766+fatfishe@users.noreply.github.com>
2025-06-09 22:51:37 -0400

    Delete 01-Sqrt-Python directory

--- 01-Sqrt-Python/sqrt_demo.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import math
-
-if __name__ == "__main__":
-
-    for power in range(0, 20):
-        result = math.sqrt(1 + 10 ** -power)
-        print(f" sqrt(1 + 10^-{power:<2d}) = {result:>50.50f}")
